56e96eef91c0ff55d02476a887417a7ac5e4d206
vince340 <154840844+vince340@users.noreply.github.com>
2024-12-14 14:42:00 +0100

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -2,31 +2,20 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-    name: 'ai',
-    description: 'Interact with GPT-4o',
-    usage: 'gpt4 [your message]',
-    author: 'coffee',
+  name: 'ai',
+  description: 'Xao Ai',
+  usage: 'aiv2 [your Question or Message]',
+  author: 'Gelie',
 
-    async execute(senderId, args, pageAccessToken) {
-        const prompt = args.join(' ');
-        if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
+  async execute(senderId, args, pageAccessToken) {
+    const prompt = args.join(' ');
+    if (!prompt) return sendMessage(senderId, { text: "Usage: aiv2 <question>" }, pageAccessToken);
 
-        try {
-            const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?q=${encodeURIComponent(prompt)}&uid=${senderId}`);
-
-            const parts = [];
-
-            for (let i = 0; i < response.length; i += 1999) {
-                parts.push(response.substring(i, i + 1999));
-            }
-
-            // send all msg parts
-            for (const part of parts) {
-                await sendMessage(senderId, { text: part }, pageAccessToken);
-            }
-
-        } catch {
-            sendMessage(senderId, { text: 'Veuillez attendre un peu s'il vous plait, vous êtes très nombreux et mon serveur est un peu surchargé. :(' }, pageAccessToken);
-        }
+    try {
+      const { data: { result } } = await axios.get(`https://api.y2pheq.me/xaoai?prompt=hello${encodeURIComponent(prompt)}&UID=${senderId}`);
+      sendMessage(senderId, { text: result }, pageAccessToken);
+    } catch {
+      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
     }
+  }
 };
