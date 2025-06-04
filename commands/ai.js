0004ad20d436176980a849bfad03040700b90ef4
vince340 <154840844+vince340@users.noreply.github.com>
2025-06-03 21:08:13 -0400

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -32,7 +32,7 @@ module.exports = {
 };
 
 const handleChatResponse = async (senderId, input, pageAccessToken) => {
-  const apiUrl = "https://api.kenliejugarap.com/freegpt4o8k/?question=${encodeURIComponent(prompt)}";
+  const apiUrl = "https://api.nekorinn.my.id/ai/gemma-3-27b?text=${encodeURIComponent(RP + " : " + prompt)}";
 
   try {
     const aidata = await axios.get(apiUrl, { params: { q: input, uid: senderId } });
