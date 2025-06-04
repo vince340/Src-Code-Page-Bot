3b400cfe6098c7630eff871377d9d8109157da2f
vince340 <154840844+vince340@users.noreply.github.com>
2025-06-03 21:00:55 -0400

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -32,7 +32,7 @@ module.exports = {
 };
 
 const handleChatResponse = async (senderId, input, pageAccessToken) => {
-  const apiUrl = "https://kaiz-apis.gleeze.com/api/bert-ai";
+  const apiUrl = "https://api.kenliejugarap.com/freegpt4o8k/?question=${encodeURIComponent(prompt)}";
 
   try {
     const aidata = await axios.get(apiUrl, { params: { q: input, uid: senderId } });
